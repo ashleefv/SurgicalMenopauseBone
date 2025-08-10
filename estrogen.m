4e6a2cff3f68c98075ca9eeeb2b71cbad085279f
Ashlee N. Ford Versypt <ashleefv@buffalo.edu>
2025-08-10 12:10:14 -0400

    t_m is menopause onset

--- estrogen.m
+++ estrogen.m
@@ -1,23 +1,23 @@
 % % time dependent estrogen function
-% function e=estrogen(t,if_surgical,t_e,tau_e,k_dec,k_syn)
+% function e=estrogen(t,if_surgical,t_m,tau_e,k_dec,k_syn)
 % 
-% % t_e is the onset point of menopause
+% % t_m is the onset point of menopause
 % 
 % % if_surgical is boolean which is 0 for natural menopause, 1 for surgical
 % % menopause
-% (t<t_e)
+% (t<t_m)
 % % for natural menopause:
 % % tau_e is the decay rate of estrogen
 % % surgical menopause:is a step function between e=1 and e=eovx
 % 
 % if if_surgical==1
-%     %  e=1.*(t<t_e)+eovx*(t>=t_e); % step function definition using embedded if statements (t<te)
+%     %  e=1.*(t<t_m)+eovx*(t>=t_m); % step function definition using embedded if statements (t<te)
 % 
-%     e=1.*(t<t_e)+(t>=t_e).*((1-k_syn/k_dec)*exp(-k_dec*(t-t_e))+k_syn/k_dec); % step function definition using embedded if statements (t<te)
+%     e=1.*(t<t_m)+(t>=t_m).*((1-k_syn/k_dec)*exp(-k_dec*(t-t_m))+k_syn/k_dec); % step function definition using embedded if statements (t<te)
 % 
 % else
 %     'hre'
-%     e=1.*(t<t_e)+(t>=t_e)/(1+ (t-t_e)/tau_e);
+%     e=1.*(t<t_m)+(t>=t_m)/(1+ (t-t_m)/tau_e);
 % end
 % 
 % end
@@ -25,9 +25,9 @@
 
 
 % time dependent estrogen function
-function e=estrogen(t,if_surgical,t_e,tau_e,k_dec,k_syn)
+function e=estrogen(t,if_surgical,t_m,tau_e,k_dec,k_syn)
 
-% t_e is the onset point of menopause
+% t_m is the onset point of menopause
 
 % if_surgical is boolean which is 0 for natural menopause, 1 for surgical
 % menopause
@@ -37,15 +37,15 @@ function e=estrogen(t,if_surgical,t_e,tau_e,k_dec,k_syn)
 % surgical menopause:is a step function between e=1 and e=eovx
 
 if if_surgical
-    %  e=1.*(t<t_e)+eovx*(t>=t_e); % step function definition using embedded if statements (t<te)
-    % e=1.*(t<t_e)+(t>=t_e).*((1-k_syn/k_dec).*exp(-k_dec.*(t-t_e))+k_syn/k_dec) % step function definition using embedded if statements (t<te)
-    if t>=t_e
-        e=((1-k_syn/k_dec).*exp(-k_dec.*(t-t_e))+k_syn/k_dec);
+    %  e=1.*(t<t_m)+eovx*(t>=t_m); % step function definition using embedded if statements (t<te)
+    % e=1.*(t<t_m)+(t>=t_m).*((1-k_syn/k_dec).*exp(-k_dec.*(t-t_m))+k_syn/k_dec) % step function definition using embedded if statements (t<t_m)
+    if t>=t_m
+        e=((1-k_syn/k_dec).*exp(-k_dec.*(t-t_m))+k_syn/k_dec);
     else
         e=1;
     end
 else
-    e=1.*(t<t_e)+(t>=t_e)./(1+ (t-t_e)/tau_e);
+    e=1.*(t<t_m)+(t>=t_m)./(1+ (t-t_m)/tau_e);
 end
 
 end
